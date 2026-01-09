aaf0d2da0afbc5c749cb9aa6b508c1bd10f3628c
Viee.codes <161074194+Vipulsnips@users.noreply.github.com>
2026-01-09 22:57:20 +0530

    Time: 0 ms (100%), Space: 21.9 MB (85.23%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int,TreeNode *> check(TreeNode* root) {
+        if (!root) return {0, NULL};
+        auto left  = check(root->left);
+        auto right = check(root->right);
+        if(left.first > right.first) {
+            return {left.first +1 ,left.second};
+        }
+        else if(left.first < right.first) return{right.first+1 ,right.second};
+        else return{left.first +1 , root};
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        auto ans = check(root).second;
+        return ans;
+    }
+};
