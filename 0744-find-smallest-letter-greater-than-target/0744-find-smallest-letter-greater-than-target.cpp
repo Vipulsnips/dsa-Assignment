87d1769e6d21ba4157d558a629aef61998e9897d
Viee.codes <161074194+Vipulsnips@users.noreply.github.com>
2026-01-31 10:58:39 +0530

    Time: 3 ms (1.89%), Space: 19.8 MB (24.67%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        sort(letters.begin(),letters.end());
+        auto it=upper_bound(letters.begin(),letters.end(),target);
+        if(it!=letters.end()) return *it;
+        else return *letters.begin();
+    }
+};
