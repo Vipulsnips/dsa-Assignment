0df4079e2044b0f9ad4cb971410298cd796b5139
Viee.codes <161074194+Vipulsnips@users.noreply.github.com>
2026-02-08 13:10:57 +0530

    Time: 9 ms (29.81%), Space: 21 MB (10.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    bool solve(unordered_map <int , vector <int > >&adj,int n,vector <int> &indeg){
-        int count=0;
-        queue <int> q;
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0){
-                q.push(i);
-                count++;
+    bool solve(unordered_map <int , vector <int > >&adj,int i,vector <bool> &visited,vector <bool> &inrecursion){
+        visited[i]=1;
+        inrecursion[i]=1;
+        for(auto it:adj[i]){
+            if(visited[it] && inrecursion[it]){
+                return true;
             }
+            if(!visited[it] && solve(adj,it,visited,inrecursion))
+                return true;
         }
-        while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-            for(auto i:adj[curr]){
-                indeg[i]--;
-                if(indeg[i]==0){
-                    q.push(i);
-                    count++;
-                }
-            }
-        }
-        return count==n;
+        inrecursion[i]=0;
+        return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map <int , vector <int > > adj;
-        vector <int> indeg(numCourses, 0);
-        for(auto i : prerequisites){
+        for(auto i:prerequisites){
             adj[i[1]].push_back(i[0]);
-            indeg[i[0]]++;
         }
-
-        return solve(adj , numCourses , indeg);
+        vector <bool> visited(numCourses, 0);
+        vector <bool> inrecursion(numCourses, 0);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i] && solve(adj,i,visited,inrecursion)) return false;
+        }
+        return true;
     }
 };
