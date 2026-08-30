ad9afc800af9fdc32bb88488a4e5991739beef83
Viee.codes <161074194+Vipulsnips@users.noreply.github.com>
2026-08-30 13:22:06 +0530

    Time: 1027 ms (9.96%), Space: 251 MB (6.57%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,34 +1,53 @@
 class Solution {
 public:
-    typedef pair<int,int> p;
-    int Prims(unordered_map<int,vector<p>>&adj,int n){
-        vector<bool> visited(n,0);
-        priority_queue<p,vector<p>,greater<p>>q;
-        q.push({0,0});int sum=0;
-        while(!q.empty()){
-            p curr=q.top();
-            q.pop();
-            if(visited[curr.second]) continue;
-            visited[curr.second]=1;
-            sum+=curr.first;
-            for(auto i:adj[curr.second]){
-                if(!visited[i.first]){
-                    q.push({i.second,i.first});
-                }
+    // USING KRUSKAL ALGORITHM
+    vector<int> parent;
+    vector<int> rank;
+    int find (int x) {
+        if (x == parent[x]) 
+            return x;
+    
+        return parent[x] = find(parent[x]);
+    }
+    void Union (int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+    
+        if (x_parent == y_parent) 
+            return;
+    
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    int Kruskal(vector<vector<int>>&edges){
+        auto comparator= [&](vector<int>&a,vector<int>&b){return a[2]<b[2];};
+        sort(edges.begin(),edges.end(),comparator);
+        int ans=0;
+        for(auto i:edges){
+            if(find(i[0])!=find(i[1])){
+                Union(i[0],i[1]);
+                ans+=i[2];
             }
         }
-        return sum;
+        return ans;
     }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_map<int,vector<p>> adj;
-        int n=points.size();
+        vector<vector<int>> edges;
+        int n=points.size();rank.resize(n,0);
+        parent.resize(n);
         for(int i=0;i<n;i++){
+            parent[i]=i;
             for(int j=i+1;j<n;j++){
                 int dist= abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                adj[i].push_back({j,dist});
-                adj[j].push_back({i,dist});
+                edges.push_back({i,j,dist});
             }
         }
-        return Prims(adj,n);
+        return Kruskal(edges);
     }
 };
